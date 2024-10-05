db56839a0d422a3801cf7f336c740cd561c8a595
Michael Ricordeau <mike@plivo.com>
2024-10-05 11:17:41 -0400

    Just run tests in the current directory

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@ redis-server --daemonize yes
 sleep 1
 case "$1" in
   "test")
-    echo "Running tests..."
-    /usr/local/go/bin/go test ./...
+    echo "Running tests in the current directory..."
+    /usr/local/go/bin/go test .
     ;;
   "benchmark"|"bench")
     echo "Running benchmarks..."
-    /usr/local/go/bin/go test -bench=. ./...
+    /usr/local/go/bin/go test -bench=. .
     ;;
   "shell"|"sh"|"bash")
     echo "Starting shell..."
